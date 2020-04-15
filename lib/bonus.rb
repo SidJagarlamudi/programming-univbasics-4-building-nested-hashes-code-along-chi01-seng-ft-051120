3e7ac64ea338a8b9109436a594200c27d4a6ec57
Sid Jagarlamudi <jaglings@gmail.com>
2020-04-15 22:18:05 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -21,7 +21,7 @@ def bonus
   }
 
   
- epic_tragedy[:capulet][:heroine][:status] = "dead"
+ bonus =epic_tragedy[:capulet][:heroine][:status] = "dead"
  epic_tragedy[:montague][:hero][:status] = "dead"
   
 
